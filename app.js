6e49e065b32d0530a58972f4a07a75fef75207e1
Faber Vergara <fvergara@elcomercio.com>
2016-03-02 11:10:00 -0500

    initial charge

---
+++ app.js
@@ -0,0 +1,5 @@
+var polyline = require('polyline');
+
+var result = polyline.decode("}qpwFxdsbMvCz@|@wC|@uCDWDSOEeGaB}Aa@yFaBcDy@sCs@h@gBtGsSPm@SKmBoAyByAkFoDoFkDz@uC");
+console.log('RESULT', result);
+
