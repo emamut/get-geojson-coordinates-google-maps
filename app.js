8be429ba8b23ebc96e52a151d4c401814cf646f2
Faber Vergara <fvergara@elcomercio.com>
2016-03-07 18:56:13 -0500

    form added

--- app.js
+++ app.js
@@ -2,23 +2,38 @@
 
 var polyline = require('polyline'),
 request = require("request"),
+express = require("express"),
+path = require("path"),
+app = express(),
 directions = {
-    'origin': [-1.304536, -78.636797],
-    'destination': [-1.722909, -78.762353]
+    'origin': [-0.280422, -78.533411],
+    'destination': [-0.258665, -78.522554]
 }
 
-request("https://maps.googleapis.com/maps/api/directions/json?origin=" + directions.origin[0] + "," + directions.origin[1] + "&destination=" + directions.destination[0] + "," + directions.destination[1], function (error, response, body) {
-    body = JSON.parse(body);
-    var coordinates = {};
-    coordinates.type = "LineString";
 
-    coordinates.coordinates = polyline.decode(body.routes[0].overview_polyline.points);
-    for(var cont = 0; cont < coordinates.coordinates.length; cont++) {
-        var temp;
-        temp = coordinates.coordinates[cont][0];
-        coordinates.coordinates[cont][0] = coordinates.coordinates[cont][1];
-        coordinates.coordinates[cont][1] = temp;
-    }
+app.post('/response', function (request, response) {
+    // request("https://maps.googleapis.com/maps/api/directions/json?origin=" + directions.origin[0] + "," + directions.origin[1] + "&destination=" + directions.destination[0] + "," + directions.destination[1], function (error, response, body) {
+    //     body = JSON.parse(body);
+    //     var coordinates = {};
+    //     coordinates.type = "LineString";
+    //
+    //     coordinates.coordinates = polyline.decode(body.routes[0].overview_polyline.points);
+    //     for(var cont = 0; cont < coordinates.coordinates.length; cont++) {
+    //         var temp;
+    //         temp = coordinates.coordinates[cont][0];
+    //         coordinates.coordinates[cont][0] = coordinates.coordinates[cont][1];
+    //         coordinates.coordinates[cont][1] = temp;
+    //     }
+    //
+    //     response.setHeader('Content-Type', 'application/json');
+    //     response.send(JSON.stringify(coordinates));
+    // });
+});
 
-    console.log(JSON.stringify(coordinates));
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000, function () {
+    console.log('Open in your browser http://localhost:3000');
 });
