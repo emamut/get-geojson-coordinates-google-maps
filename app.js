0e8f80f2c46b3f859aedbfb9e2963fcd97f988f3
Faber Vergara <fvergara@elcomercio.com>
2016-03-09 11:58:06 -0500

    prompt added

--- app.js
+++ app.js
@@ -2,25 +2,30 @@
 
 var polyline = require('polyline'),
 request = require("request"),
+prompt = require('prompt');
 
-directions = {
-    'origin': [-0.280422, -78.533411],
-    'destination': [-0.258665, -78.522554]
-}
+prompt.start();
 
+prompt.get(['origin', 'destination'], function (err, result) {
+    if (err) { return onErr(err); }
+    request("https://maps.googleapis.com/maps/api/directions/json?origin=" + result.origin + "&destination=" + result.destination, function (error, response, body) {
+        body = JSON.parse(body);
+        var coordinates = {};
+        coordinates.type = "LineString";
 
-request("https://maps.googleapis.com/maps/api/directions/json?origin=" + directions.origin[0] + "," + directions.origin[1] + "&destination=" + directions.destination[0] + "," + directions.destination[1], function (error, response, body) {
-    body = JSON.parse(body);
-    var coordinates = {};
-    coordinates.type = "LineString";
+        coordinates.coordinates = polyline.decode(body.routes[0].overview_polyline.points);
+        for(var cont = 0; cont < coordinates.coordinates.length; cont++) {
+            var temp;
+            temp = coordinates.coordinates[cont][0];
+            coordinates.coordinates[cont][0] = coordinates.coordinates[cont][1];
+            coordinates.coordinates[cont][1] = temp;
+        }
 
-    coordinates.coordinates = polyline.decode(body.routes[0].overview_polyline.points);
-    for(var cont = 0; cont < coordinates.coordinates.length; cont++) {
-        var temp;
-        temp = coordinates.coordinates[cont][0];
-        coordinates.coordinates[cont][0] = coordinates.coordinates[cont][1];
-        coordinates.coordinates[cont][1] = temp;
-    }
-
-    console.log(JSON.stringify(coordinates));
+        console.log(JSON.stringify(coordinates));
+    });
 });
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
