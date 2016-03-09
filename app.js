2bb7f9672378d4f2aa215a92d928ecedcf83075b
Faber Vergara <fvergara@elcomercio.com>
2016-03-09 15:27:59 -0500

    form added

--- app.js
+++ app.js
@@ -2,36 +2,37 @@
 
 var polyline = require('polyline'),
 request = require("request"),
-express = require("express"),
-path = require("path"),
-app = express(),
-directions = {
-    'origin': [-0.280422, -78.533411],
-    'destination': [-0.258665, -78.522554]
-}
+app = require("express")(),
+bodyParser = require('body-parser'),
+fs = require("fs");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/response', function (request, response) {
-    // request("https://maps.googleapis.com/maps/api/directions/json?origin=" + directions.origin[0] + "," + directions.origin[1] + "&destination=" + directions.destination[0] + "," + directions.destination[1], function (error, response, body) {
-    //     body = JSON.parse(body);
-    //     var coordinates = {};
-    //     coordinates.type = "LineString";
-    //
-    //     coordinates.coordinates = polyline.decode(body.routes[0].overview_polyline.points);
-    //     for(var cont = 0; cont < coordinates.coordinates.length; cont++) {
-    //         var temp;
-    //         temp = coordinates.coordinates[cont][0];
-    //         coordinates.coordinates[cont][0] = coordinates.coordinates[cont][1];
-    //         coordinates.coordinates[cont][1] = temp;
-    //     }
-    //
-    //     response.setHeader('Content-Type', 'application/json');
-    //     response.send(JSON.stringify(coordinates));
-    // });
+app.post('/response', function (req, res) {
+    request("https://maps.googleapis.com/maps/api/directions/json?origin=" + req.body.origin + "&destination=" + req.body.destination, function (error, response, body) {
+        body = JSON.parse(body);
+        var coordinates = {};
+        coordinates.type = "LineString";
+
+        coordinates.coordinates = polyline.decode(body.routes[0].overview_polyline.points);
+        for(var cont = 0; cont < coordinates.coordinates.length; cont++) {
+            var temp;
+            temp = coordinates.coordinates[cont][0];
+            coordinates.coordinates[cont][0] = coordinates.coordinates[cont][1];
+            coordinates.coordinates[cont][1] = temp;
+        }
+
+        // res.setHeader('Content-Type', 'application/json');
+        res.send(coordinates);
+    });
 });
 
 app.get('/', function (request, response) {
-    response.sendFile(path.join(__dirname + '/index.html'));
+    fs.readFile('index.html', function(err, page) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(page);
+        response.end();
+    });
 });
 
 app.listen(3000, function () {
